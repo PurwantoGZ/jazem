a82f36119d07c8f65295f2142e6b560e42d2e428
Purwanto <purwanto@outlook.com>
2019-06-23 22:04:54 +0700

    create hello.go

---
+++ hello.go
@@ -0,0 +1,11 @@
+package jazem
+
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+)
+
+func hello_ku(word string) {
+  fmt.Println(word)
+	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
+}
